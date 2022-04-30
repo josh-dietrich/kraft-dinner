76cf87e109457666411ba50a4d8bd62675f8e7ba
Joshua R. Dietrich <104660269+josh-dietrich@users.noreply.github.com>
2022-04-29 20:58:52 -0400

    Create i-am-going-to-leave-this-call-now.py

---
+++ i-am-going-to-leave-this-call-now.py
@@ -0,0 +1,9 @@
+import os
+import scipy.integrate
+import tensorflow as tf
+from tensorflow.python.keras.layers import Input, Dense
+import cv2
+import pyautogui
+import selenium
+
+print("kraft dinner")
